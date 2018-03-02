4c320867dbfb35db83574ccc3bac1b6b066c86f7
unknown <crawford7100@gmail.com>
2018-03-01 20:50:25 -0500

    first

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
 # liri-node-app
+
+#Introduction
+
+You already know SIRI now meet LIRI(Language Interpretation and Recognition Interface)! SIRI for your command line!
+
+Run LIRI from your command line 
+    *To Start cd into the target directory
+    *type "node liri.js" to get started
+
+LIRI can perform the following functions. 
+    *My-Tweets
+    *Spotify-This-Song
+    *Movie-This
+    *Do-What-It-Says
+
+!Required NPM Packages!
+    *fs package(node)
+    *twitter
+    *spotify
+    *request
+
---
+++ liri.js
@@ -0,0 +1,130 @@
+//Required NPMs//
+require("dotenv").config();
+var fs = require("fs");
+var request = require("request");
+var request = require("./keys.js");
+var twitter = require("twitter");
+var spotify = require("spotify");
+var liriArgument = process.argv[2];
+
+//Liri Commands//
+switch(liriArgument) {
+    case "my-tweets": myTweets(); break;
+    case "spotify-this-song": spotifyThisSong(); break;
+    case "movie-this": movieThis(); break;
+    case "do-what-it-says": doWhatItSays(); break;
+//User Instructions//
+default: console.log("\r\n" +"Choose from the following list of commands" + "\r\n" +
+    "1. my-tweets 'any twitter name' " +"\r\n"+
+	"2. spotify-this-song 'any song name' "+"\r\n"+
+	"3. movie-this 'any movie name' "+"\r\n"+
+	"4. do-what-it-says."+"\r\n"+
+    "Using more than one word? Put it in quotation marks.");
+};
+
+//Functions//
+
+//Movie Function//
+function movieThis(){
+    var movie = process.argv[3];
+    if(!movie){
+        movie = "Mr. Nobody";
+    }
+    params = movie 
+    request("http://www.omdbapi.com/?t=" + params + "&y=&plot=short&r=json&tomatoes=true", function (error, response, body) {
+			if (!error && response.statusCode == 200) {
+				var movieObject = JSON.parse(body);
+				var movieResults =
+				"------------------------------------------------------------" + "\r\n"
+				"Title: " + movieObject.Title+"\r\n"+
+				"Year: " + movieObject.Year+"\r\n"+
+				"Imdb Rating: " + movieObject.imdbRating+"\r\n"+
+				"Country: " + movieObject.Country+"\r\n"+
+				"Language: " + movieObject.Language+"\r\n"+
+				"Plot: " + movieObject.Plot+"\r\n"+
+				"Actors: " + movieObject.Actors+"\r\n"+
+				"Rotten Tomatoes Rating: " + movieObject.tomatoRating+"\r\n"+
+				"Rotten Tomatoes URL: " + movieObject.tomatoURL + "\r\n" + 
+				"------------------------------------------------------------" + "\r\n";
+				console.log(movieResults);
+				log(movieResults);
+			} else {
+				console.log("Error :"+ error);
+				return;
+			}
+	});
+};
+
+//Spotify Function//
+
+function spotifyThisSong(songName) {
+    var songName = process.argv[3];
+    if(!songName){
+        songName = "What's my age again";
+    }
+    params = songName;
+    spotify.search({ type: "track", query: params }, function(err, data) {
+        if(!err){
+            var songInfo = data.tracks.items;
+            for (var i = 0; i < 5; i++) {
+                if (songInfo[i] != undefined) {
+                    var spotifyResults =
+                    "Artist: " + songInfo[i].artists[0].name + "\r\n" +
+                    "Song: " + songInfo[i].name + "\r\n" +
+                    "Album the song is from: " + songInfo[i].album.name + "\r\n" +
+                    "Preview Url: " + songInfo[i].preview_url + "\r\n" + 
+                    "------------------------------ " + i + " ------------------------------" + "\r\n";
+                    console.log(spotifyResults);
+                    log(spotifyResults); // calling log function
+                }
+            }
+        }	else {
+            console.log("Error :"+ err);
+            return;
+        }
+    });
+};
+
+//Twitter Function//
+
+function myTweets() {
+    var client = new twitter({
+        consumer_key: keys.twitterKeys.consumer_key,
+        consumer_secret: keys.twitterKeys.consumer_secret,
+        access_token_key: keys.twitterKeys.access_token_key,
+        access_token_secret: keys.twitterKeys.access_token_secret, 
+    });
+    var twitterUsername = process.argv[3];
+    if(!twitterUsername){
+        twitterUsername = "JahdashaFlagg";
+    }
+    params = {screen_name: twitterUsername};
+    client.get("statuses/user_timeline/", params, function(error, data, response){
+        if (!error) {
+            for(var i = 0; i < data.length; i++) {
+                //console.log(response); // Show the full response in the terminal
+                var twitterResults = 
+                "@" + data[i].user.screen_name + ": " + 
+                data[i].text + "\r\n" + 
+                data[i].created_at + "\r\n" + 
+                "------------------------------ " + i + " ------------------------------" + "\r\n";
+                console.log(twitterResults);
+                log(twitterResults); // calling log function
+            }
+        }  else {
+            console.log("Error :"+ error);
+            return;
+        }
+    });
+}
+
+//Do What it Says Function//
+
+function log(logResults) {
+    fs.appendFile("log.txt", logResults, (error) => {
+      if(error) {
+        throw error;
+      }
+    });
+  }
+
